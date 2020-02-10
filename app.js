2e93fba42fb309dd44492db8363c295c7cbb42cd
Fernanda <fernandaromero85@gmail.com>
2020-02-10 14:21:31 -0500

    Function version 1

--- app.js
+++ app.js
@@ -1,1 +1,101 @@
+$(document).ready( function () {
+var playerAnswer;
+var gameAnswer;
+var time;
+var counter = 10;
+var score = 0;
 
+  // timer function
+  var timer = function () {
+    if (!time) {
+      if (counter === 0) {
+        bonusTime(10);
+        updateScore(-score);
+      }
+      time = setInterval(function () {
+        bonusTime(-1);
+        if (counter === 0) {
+          clearInterval(interval);
+          time = undefined;
+        }
+      }, 1000);
+    }
+  };
+
+  // bonus time
+  var bonusTime = function (bonus) {
+    counter += bonus;
+    $('#time').text(counter);
+  };
+
+  // method selected
+    $(".btn-operator").click(function() {
+      var method = $(this).attr('id');
+      if (method === "add") {
+        $('#method').text("+");
+      } else if (method === "sub") {
+        $('#method').text("-");
+      } else if (method === "mult") {
+        $('#method').text("x");
+      } else if (method === "div") {
+        $('#method').text("÷");
+      }
+
+    });
+
+// generate random questions
+var generator = function () {
+
+  $('#playerAnswer').val('');
+
+  var num1 = 1 + Math.round(9 * Math.random());
+	var num2 = 1 + Math.round(9 * Math.random());
+  var method = $("#method").text();
+
+  $('#num1').html(num1);
+  $('#num2').html(num2);
+
+  if (method === "+") {
+      gameAnswer = num1 + num2;
+
+    } else if (method === "-" && num2 < num1) {
+      gameAnswer = num1 - num2;
+
+    } else if (method === "x") {
+      gameAnswer = num1 * num2;
+
+    } else if (method === "÷" && num2 < num1) {
+      gameAnswer = num1 / num2;
+    }
+};
+
+// validate correct answer
+var checkAnswer = function(key) {
+    if (key.which == 13) {
+      playerAnswer = $('#playerAnswer').val();
+      if (playerAnswer != '') {
+        playerAnswer = parseInt(playerAnswer);
+        // Check answer
+        if (playerAnswer == gameAnswer) {
+          generator();
+          bonusTime(+1);
+        }
+      }
+    }
+  };
+
+// validate correct answer
+$("input").keypress(function(key) {
+  timer();
+  checkAnswer(key);
+  });
+
+
+$("#play").on("click", function () {
+  timer();
+  generator();
+
+});
+
+
+});
